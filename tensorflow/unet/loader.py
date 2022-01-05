4f4bfe85a4932d6725f27c6944e2cc67e124d4a9
AmbroiseM <74647597+AmbroiseM@users.noreply.github.com>
2022-01-05 03:17:52 +0100

    Update loader.py

--- tensorflow/unet/loader.py
+++ tensorflow/unet/loader.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Dec  7 20:33:17 2021
-
-@author: Ambroise M
-"""
 
 import tensorflow as tf
 import numpy as np
-import cv2
 from PIL import Image
 
 import imageio
@@ -15,44 +9,40 @@ import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
 
-img_path = r'C:/Users/Ambroise M/Desktop/archive/images/images/'
-mask_path = r'C:/Users/Ambroise M/Desktop/archive/annotations/annotations/trimaps/'
-
 
 class DataGen(tf.keras.utils.Sequence):
     
-    def __init__(self, img_size, img_path, mask_path, batch_size=32): 
+    def __init__(self, img_size, mask_size, imgs, masks, batch_size=16): 
         
         self.img_size = img_size
-        
-        self.imgs = [f for f in listdir(img_path) 
-                          if (isfile(join(img_path,f)) and f.endswith(".jpg"))]
-        
-        self.masks = [f for f in listdir(mask_path) 
-                          if (isfile(join(mask_path,f)) and f.endswith(".png") and not f.startswith("."))]
-        
-        self.mask_path = mask_path
+        self.mask_size = mask_size
+        self.imgs = imgs         
+        self.masks = masks
         self.batch_size = batch_size
         
+                
     def __len__(self):
-        return len(self.img_path)//self.batch_size
+        return len(self.imgs)//self.batch_size
     
     def __getitem__(self, index):
+
         img_batch = self.imgs[ index*self.batch_size:(index + 1)*self.batch_size]
         mask_batch = self.masks[ index*self.batch_size:(index + 1)*self.batch_size]
         
-        # x =
-        # y = 
-        for image, mask in zip(img_batch, mask_batch):
-           
-            img = imageio.imread(img_path + '/' + image)
-            img = np.resize(img,(self.img_size))
-            
-            msk = imageio.imread(mask_path + '/' + mask)
-            msk = np.resize(img,self.mask_size)# print(tf.shape(img))
-            print(img.shape)
-            
+        #x, y =[], []
+        #for image, mask in zip(img_batch, mask_batch):
 
-unet_data = DataGen((100,100,3), img_path, mask_path)
+        x = np.zeros((self.batch_size,) + self.img_size, dtype="float32")
+        for j, path in enumerate(img_batch):
+          img = tf.keras.preprocessing.image.load_img(img_path + '/'+path, target_size=self.img_size)
+          x[j] = img
 
-unet_data.__getitem__(1)
+        y = np.zeros((self.batch_size,) + self.mask_size, dtype="uint8")
+        for j, path in enumerate(mask_batch):
+          img = tf.keras.preprocessing.image.load_img(mask_path + '/'+path, target_size=self.img_size, color_mode="grayscale")
+          y[j] = np.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
+          y[j] -= 1
+        
+        return x, y
+            
